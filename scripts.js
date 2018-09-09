3092e5299d8335cd57064ff1c2beec2d042f06ee
MichalIrzylowski <michal.irzylowski@gmail.com>
2018-09-09 18:08:24 +0200

    add pie charts

--- scripts.js
+++ scripts.js
@@ -1,5 +1,30 @@
 const burger = document.querySelector(".burger");
+const progressBars = document.querySelectorAll(".chart");
 
 burger.addEventListener("click", function() {
   this.classList.toggle("active");
+});
+
+new EasyPieChart(progressBars[0], {
+  barColor: "#f34739",
+  scaleColor: false,
+  lineWidth: 4,
+  size: 220,
+  rotate: 160
+});
+
+new EasyPieChart(progressBars[1], {
+  barColor: "#009989",
+  scaleColor: false,
+  lineWidth: 4,
+  size: 220,
+  rotate: 160
+});
+
+new EasyPieChart(progressBars[2], {
+  barColor: "#152b3c",
+  scaleColor: false,
+  lineWidth: 4,
+  size: 220,
+  rotate: 160
 });
